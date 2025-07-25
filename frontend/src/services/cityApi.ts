800a9a41065c5bd9c071fba67383d02551909c35
andremelzc <andre.melendezc@unmsm.edu.pe>
2025-07-25 12:53:36 -0500

    featuure/ service cityApi

---
+++ frontend/src/services/cityApi.ts
@@ -0,0 +1,11 @@
+const PORT_BACKEND = import.meta.env.PORT_BACKEND || 3000;
+const API_BASE =
+  import.meta.env.VITE_API_BASE || `http://localhost:${PORT_BACKEND}`;
+
+export const getCities = async (query: string) => {
+  const response = await fetch(`${API_BASE}/api/cities/search?q=${query}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch city data");
+  }
+  return response.json();
+};
